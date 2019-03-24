a643e97b01ecfdcc29e2f69f2b77d8229a0cdc17
shhuang123 <sharonhuang923@gmail.com>
2019-03-24 07:09:15 +0000

    lessons permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
+
+
   def show
   end
 
 private
+
+ def require_authorized_for_current_section
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: "Must enrolled to view the page"
+    end
+ end 
 
   helper_method :current_lesson
   def current_lesson
