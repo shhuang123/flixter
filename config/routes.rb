230b8166926e99c8435d07b25b14b14751c6cedc
shhuang123 <sharonhuang923@gmail.com>
2019-03-24 04:13:13 +0000

    student enrollment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
@@ -11,4 +13,4 @@ Rails.application.routes.draw do
       resources :sections, only: [:new, :create]
     end
   end
-end 
+end
